415c5d8f0c8e58f32da62e105e7c191838cb7ce3
Peter <peathal@yahoo.de>
2009-10-05 14:41:20 +0200

    * deploy to my m2 repos

--- deploy2maven.sh
+++ deploy2maven.sh
@@ -1,3 +1,4 @@
 #mvn install:install-file -DgroupId=de.pannous.xvantage -DartifactId=xvantage-core -Dversion=0.8 -Dpackaging=jar -Dfile=dist/Xvantage.jar
 mvn clean install -Dmaven.test.skip=true
+scp ~/.m2/repository/de/pannous/xvantage/xvantage-core/0.8/* peat_hal@web.sourceforge.net:/home/users/p/pe/peat_hal/userweb/htdocs/m2repository/de/pannous/xvantage/xvantage-core/0.8/
 mvn clean
